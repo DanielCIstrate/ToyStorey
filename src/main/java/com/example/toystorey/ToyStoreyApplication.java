aeac87abf78bc13f7319e111e7042fa8e2956082
DanielCIstrate <daniel.istr8@gmail.com>
2021-09-29 13:45:45 +0300

    project initialization

---
+++ src/main/java/com/example/toystorey/ToyStoreyApplication.java
@@ -0,0 +1,13 @@
+package com.example.toystorey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyStoreyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyStoreyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toystorey/ToyStoreyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toystorey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyStoreyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
