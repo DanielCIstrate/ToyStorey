a5e2b6b8b97da192b5454e3d88b50e5d3b2a0cb9
DanielCIstrate <daniel.istr8@gmail.com>
2021-10-13 10:27:03 +0300

    ADD Navbar menu
    includes:
    mobile friendly navbar-menu with toggle burger icon click

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bulma/css/bulma.min.css';
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
     </div>
   );
 }
--- react-app/src/Components/Navbar.js
+++ react-app/src/Components/Navbar.js
@@ -1,13 +1,48 @@
 import logo from "../assets/Logo.jpg"
+import {useState} from "react";
 
 const Navbar = () => {
-    const logoStyle = {maxHeight: "20vh"}
+    const logoStyle = {maxHeight: "20vh"};
+    // const burgerIcon = document.querySelector('#burger');
+    // const navbarMenu = document.querySelector('#nav-links');
+    // burgerIcon.addEventListener('click', () => {
+    //     navbarMenu.classList.toggle('is-active');
+    // });
+    const [navbarMenuAddOnClass, setNavbarMenuAddOnClass] = useState("");
+    function toggleNavbarBurgerMenu(event) {
+        if (navbarMenuAddOnClass === "") {
+            setNavbarMenuAddOnClass("is-active");
+        } else {
+            setNavbarMenuAddOnClass("");
+        }
+    }
     return (
-        <div className="navbar">
+        <div className="navbar has-shadow is-white">
             <div className="navbar-brand">
                 <a href={"/"} className="navbar-item">
                     <img src={logo} alt="brand logo" style={logoStyle}/>
                 </a>
+
+                <a className="navbar-burger" id="burger"
+                    onClick={toggleNavbarBurgerMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+            </div>
+
+            <div className={"navbar-menu" + " " + navbarMenuAddOnClass } id="nav-links">
+                <div className="navbar-end">
+                    <a className="navbar-item">
+                        My Account
+                    </a>
+                    <a className="navbar-item">
+                        Shopping Cart (0)
+                    </a>
+                    <a className="navbar-item">
+                        About
+                    </a>
+                </div>
             </div>
         </div>
     );
