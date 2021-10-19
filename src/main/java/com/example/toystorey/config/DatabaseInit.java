6efe1d22eee23a1a4707b0a797dd7bcdf27f56f0
DanielCIstrate <daniel.istr8@gmail.com>
2021-10-19 23:16:31 +0300

    EDIT populating database with new products mock data

--- src/main/java/com/example/toystorey/config/DatabaseInit.java
+++ src/main/java/com/example/toystorey/config/DatabaseInit.java
@@ -1,42 +1,36 @@
 package com.example.toystorey.config;
 
-import com.example.toystorey.model.Product;
-import com.example.toystorey.model.ToyImage;
-import com.example.toystorey.repositories.ImageRepository;
+import com.example.toystorey.domain.Product;
+import com.example.toystorey.domain.ToyProduct;
 import com.example.toystorey.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Component
 public class DatabaseInit implements ApplicationRunner {
 
-
-    private ImageRepository imageRepository;
     private ProductRepository productRepository;
 
     @Autowired
-    public DatabaseInit(ImageRepository imageRepository, ProductRepository productRepository) {
-        this.imageRepository = imageRepository;
+    public DatabaseInit(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
-        Product tricicletaAlba = new Product("Tricicleta alba",329.99);
-        Product tricletaRosie = new Product("Tricicleta rosie", 159.99);
-        ToyImage pozica = new ToyImage();
-        ToyImage poza2 = new ToyImage();
-        tricicletaAlba.setToyImage(pozica);
-        tricletaRosie.setToyImage(poza2);
+        Product bowlingSet = new ToyProduct(null,"Bowling set", 80.00,4);
+        Product doctorSet = new ToyProduct(null,"Set doctor", 125.00, 6);
+        Product cardSet = new ToyProduct(null,"Playing cards", 20.00,7);
 
-        productRepository.save(tricicletaAlba);
-        productRepository.save(tricletaRosie);
+        productRepository.save(bowlingSet);
+        productRepository.save(doctorSet);
+        productRepository.save(cardSet);
+
 
 
 
