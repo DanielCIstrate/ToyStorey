b765d6d96df3cd3163c1dab9792ee52816e4f903
DanielCIstrate <daniel.istr8@gmail.com>
2021-10-19 23:15:19 +0300

    EDIT commented text regarding implementing the fetch for Products

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,14 +3,26 @@ import 'bulma/css/bulma.min.css';
 import Navbar from "./Components/Navbar";
 import ProductInformation from "./Components/ProductInformation";
 import DeliveryDetails from "./Components/DeliveryDetails";
+import MessageBox from "./Components/MessageBox";
+import {useState, useEffect} from "react";
 
 
 function App() {
+    const [product,setProduct] = useState([]);
+    // const fetchProduct = async() => {
+    //     await fetch("http://localhost:8080/products")
+    //         .then(response => response.json())
+    //         .then(receiveData => setProduct(receiveData))
+    // }
+    // useEffect(() => {fetchProduct();},[])
+
+
   return (
     <div className="App">
       <Navbar/>
       <ProductInformation/>
       <DeliveryDetails/>
+
     </div>
   );
 }
