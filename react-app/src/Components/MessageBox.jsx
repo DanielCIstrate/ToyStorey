1241abf804b72fd76ef056fcfab9a55e1b953aec
DanielCIstrate <daniel.istr8@gmail.com>
2021-10-19 23:19:08 +0300

    ADD MessageBox.jsx component

---
+++ react-app/src/Components/MessageBox.jsx
@@ -0,0 +1,15 @@
+
+const MessageBox = () => {
+return (
+  <div className="message is-dark">
+      <div className="message-header">
+          <p className="is-size-6">Keep in touch with discounts by bookmarking the website</p>
+      </div>
+      <div className="message-body">
+          <p className="is-size-7">Here will be some other text but for the purpose of this demo only this is shown</p>
+      </div>
+  </div>
+);
+};
+
+export default MessageBox;
