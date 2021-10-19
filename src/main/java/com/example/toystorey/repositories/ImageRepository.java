b0ac01e452218e75bd003cd44770276c17d8a2bb
DanielCIstrate <daniel.istr8@gmail.com>
2021-10-19 23:17:34 +0300

    DELETE obsolete files removed

--- src/main/java/com/example/toystorey/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.toystorey.repositories;
-
-import com.example.toystorey.model.ToyImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<ToyImage,Long> {
-}
