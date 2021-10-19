2a724e84c641a38727e5331c34ecef9d12ac563e
DanielCIstrate <daniel.istr8@gmail.com>
2021-10-19 23:16:59 +0300

    ADD test link for new image source

--- react-app/src/Components/DeliveryDetails.jsx
+++ react-app/src/Components/DeliveryDetails.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import MessageBox from "./MessageBox";
 
 const DeliveryDetails = () => {
     const [activeTab,setActiveTab] = useState(0);
@@ -18,12 +19,12 @@ const DeliveryDetails = () => {
                     <div className="column is-7-tablet">
                         <div className="tabs is-boxed">
                             <ul>
-                                <li onClick={() => handleClickActiveTab(0)} className={activeTab == 0 ? activeProperty : "" } >
+                                <li onClick={() => handleClickActiveTab(0)} className={activeTab === 0 ? activeProperty : "" } >
                                     <a>
                                       Product Details
                                     </a>
                                 </li>
-                                <li onClick={() => handleClickActiveTab(1)} className={activeTab == 1 ? activeProperty : "" }>
+                                <li onClick={() => handleClickActiveTab(1)} className={activeTab === 1 ? activeProperty : "" }>
                                     <a>
                                         Delivery Details
                                     </a>
@@ -37,12 +38,12 @@ const DeliveryDetails = () => {
                             </div>
                             <div id="delivery-information" className={activeTab == 1 ? hiddenProperty : ""}>
                                 <h3 className="is-size-5 title">Delivery Information</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi cupiditate deleniti dolorum, facere harum libero molestiae pariatur quam temporibus.</p>
+                                <p>Some other text will go here. Some other text will go here. Some other text will go here. Some other text will go here. Some other text will go here. Some other text will go here. Some other text will go here.</p>
                             </div>
                         </div>
                     </div>
                     <div className="column is-5-tablet">
-
+                        <MessageBox/>
                     </div>
 
                 </div>
